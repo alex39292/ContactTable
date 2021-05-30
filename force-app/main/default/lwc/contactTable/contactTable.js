8f19b5a29a5b4d184f7d90d19423cff25a2c06ba
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-05-30 21:40:56 +0300

    commit stage between input

--- force-app/main/default/lwc/contactTable/contactTable.js
+++ force-app/main/default/lwc/contactTable/contactTable.js
@@ -1,14 +1,17 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
 export default class ContactTable extends LightningElement {
   inputField;
+  trLength;
 
   getName() {
     this.inputField = this.template.querySelector(".input").value;
   }
 
+  @api
   filterByName() {
-    const tr = this.template.querySelectorAll(".table-tr");
+    const tr = this.template.querySelectorAll(".table-tr-body");
+    this.trLength = tr.length;
     if (!this.inputField || this.inputField === "") {
       tr.forEach((element) => {
         element.style.display = "";
