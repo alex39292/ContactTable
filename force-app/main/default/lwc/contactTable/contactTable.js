83be8d396aef783581c7deb26fad90470cd0715a
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-11-16 00:47:34 +0300

    add keypress event

--- force-app/main/default/lwc/contactTable/contactTable.js
+++ force-app/main/default/lwc/contactTable/contactTable.js
@@ -3,6 +3,12 @@ import { LightningElement } from 'lwc';
 export default class ContactTable extends LightningElement {
     inputField = '';
 
+    handleKey(event) {
+        if (event.keyCode === 13) {
+            this.handleClick();
+        }
+    }
+
     handleClick() {
         this.inputField = this.template.querySelector('.inputFilter').value;
     }
